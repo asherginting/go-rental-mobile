91f25443d2ec552d9d584a237cfe98b229355709
asherginting <asherginting3@gmail.com>
2022-04-14 08:22:46 +0700

    feat: add component input

---
+++ src/components/Input.js
@@ -0,0 +1,100 @@
+import React, {useState} from 'react';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import COLORS from './Colors';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+const Input = ({
+  label,
+  iconName,
+  error,
+  password,
+  onFocus = () => {},
+  ...props
+}) => {
+  const [hidePassword, setHidePassword] = useState(password);
+  const [isFocused, setIsFocused] = useState(false);
+  return (
+    <View style={style.view}>
+      <Text style={style.label}>{label}</Text>
+      <View
+        style={[
+          style.inputContainer,
+          {
+            borderColor: error
+              ? COLORS.red
+              : isFocused
+              ? COLORS.darkBlue
+              : COLORS.light,
+            alignItems: 'center',
+          },
+        ]}>
+        <Icon
+          name={iconName}
+          style={style.icon}
+        />
+        <TextInput
+          autoCorrect={false}
+          onFocus={() => {
+            onFocus();
+            setIsFocused(true);
+          }}
+          onBlur={() => setIsFocused(false)}
+          secureTextEntry={hidePassword}
+          style={style.textInput}
+          {...props}
+        />
+        {password && (
+          <Icon
+            onPress={() => setHidePassword(!hidePassword)}
+            name={hidePassword ? 'eye-off-outline' : 'eye-outline'}
+            style={style.eyePassword}
+          />
+        )}
+      </View>
+      {error && (
+        <Text style={style.errorMsg}>
+          {error}
+        </Text>
+      )}
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  view: {
+    marginBottom: 20,
+  },
+  label: {
+    marginVertical: 5,
+    fontSize: 14,
+    color: COLORS.grey,
+  },
+  inputContainer: {
+    height: 55,
+    backgroundColor: COLORS.light,
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    borderWidth: 0.5,
+    borderRadius: 10,
+    opacity: 0.5,
+  },
+  icon: {
+    color: COLORS.darkBlue, 
+    fontSize: 22, 
+    marginRight: 10,
+  },
+  textInput: {
+    flex: 1,
+    color: COLORS.darkBlue, 
+  },
+  eyePassword: {
+    color: COLORS.darkBlue, 
+    fontSize: 22,
+  },
+  errorMsg: {
+    marginTop: 7, 
+    color: COLORS.red, 
+    fontSize: 12,
+  },
+});
+
+export default Input;
