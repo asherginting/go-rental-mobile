e11822db2d3219f0f8a9adb81c87319e4429d6cd
asherginting <asherginting3@gmail.com>
2022-04-19 14:20:36 +0700

    feat: add profile

---
+++ src/screens/Faq.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Faq = () => {
+  return (
+    <View>
+      <Text>Faq</Text>
+    </View>
+  )
+}
+
+export default Faq
