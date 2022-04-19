5ed36f24e5d7b797a67a11cd7247939405d82b7a
asherginting <asherginting3@gmail.com>
2022-04-19 13:19:42 +0700

    feat: add teks bottom icon

--- src/navigation/BottomStackNav.js
+++ src/navigation/BottomStackNav.js
@@ -1,32 +1,33 @@
 import React from 'react';
+import { View, Text } from 'react-native'
 import HomeStackNav from './HomeStackNav'
-import HistoryStackNav from './HistoryStackNav'
+import UpStackNav from './UpStackNav'
 import Search from '../screens/Search'
 import Profile from '../screens/Profile'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const BottomTab = createBottomTabNavigator()
 
 const BottomStackNav = () => {
   return (
-    <BottomTab.Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false, tabBarActiveTintColor: '#5C527F' }}>
+    <BottomTab.Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false, tabBarActiveTintColor: '#0085DF' }}>
 
       <BottomTab.Screen options={{
-        tabBarIcon: ({ focused, color, size }) => <Icon name='home' color={color} size={size} />
+        tabBarIcon: ({ focused, color, size }) => <><Icon name='home-circle' color={color} size={30} /><Text>Home</Text></>
       }} name='HomeStackNav' component={HomeStackNav} />
 
       <BottomTab.Screen options={{
-        tabBarIcon: ({ focused, color, size }) => <Icon name='search' color={color} size={size} />
+        tabBarIcon: ({ focused, color, size }) => <><Icon name='magnify' color={color} size={30} /><Text>Search</Text></>
       }} name='Search' component={Search} />
 
       <BottomTab.Screen options={{
-        tabBarIcon: ({ focused, color, size }) => <Icon name='th-list' color={color} size={size} />
-      }} name='HistoryTab' component={HistoryStackNav} />
+        tabBarIcon: ({ focused, color, size }) => <><Icon name='view-list' color={color} size={30} /><Text>List</Text></>
+      }} name='HistoryTab' component={UpStackNav} />
 
       <BottomTab.Screen options={{
-        tabBarIcon: ({ focused, color, size }) => <Icon name='user' color={color} size={size} />
+        tabBarIcon: ({ focused, color, size }) => <><Icon name='account-circle' color={color} size={30} /><Text>Profile</Text></>
       }} name='Profile' component={Profile} />
 
     </BottomTab.Navigator>
