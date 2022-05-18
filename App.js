f8ef80693dc922e9b4aabd3871d667efef2f7f28
Asher Azriel Ginting <asherginting3@gmail.com>
2022-05-18 22:37:03 +0700

    feat: test notif from firebase

--- App.js
+++ App.js
@@ -5,28 +5,27 @@ import {NavigationContainer} from '@react-navigation/native';
 
 import {Provider, useSelector} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
-// import messaging from '@react-native-firebase/messaging';
+import messaging from '@react-native-firebase/messaging';
 
 import reduxStore from './src/redux/store';
 
 import MainStackNav from './src/navigation/MainStackNav';
 import AuthStackNav from './src/navigation/AuthStackNav';
 
-// import PushNotification from 'react-native-push-notification';
+import PushNotification from 'react-native-push-notification';
 
-// PushNotification.createChannel({
-//   channelId: 'transaction',
-//   channelName: 'transaction notification',
-//   soundName: 'default',
-//   vibrate: true,
-// });
+PushNotification.createChannel({
+  channelId: 'transaction',
+  channelName: 'transaction notification',
+  soundName: 'default',
+  vibrate: true,
+});
 
 const Main = () => {
   const { auth } = useSelector(state => state);
   return (
     <NavigationContainer>
       <NativeBaseProvider>
-        {/* <MainStackNav/> */}
         {auth.token ? <MainStackNav /> : <AuthStackNav />}
       </NativeBaseProvider>
     </NavigationContainer>
