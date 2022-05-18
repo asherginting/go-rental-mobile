ec60583148e35418ab09b8e01fb3cdd3a25966b9
Asher Azriel Ginting <asherginting3@gmail.com>
2022-05-18 11:12:16 +0700

    feat: add main order

--- src/navigation/MainStackNav.js
+++ src/navigation/MainStackNav.js
@@ -14,6 +14,7 @@ import AddVehicle from '../screens/AddVehicle'
 import ChatList from '../screens/ChatList'
 import Faq from '../screens/Faq'
 import Help from '../screens/Help'
+import Order from '../screens/Order'
 
 const MainStack = createNativeStackNavigator()
 
@@ -33,6 +34,7 @@ const MainStackNav = () => {
       <MainStack.Screen name='ChatList' component={ChatList} />
       <MainStack.Screen name='Faq' component={Faq} />
       <MainStack.Screen name='Help' component={Help} />
+      <MainStack.Screen name='Order' component={Order} />
     </MainStack.Navigator>
   )
 }
